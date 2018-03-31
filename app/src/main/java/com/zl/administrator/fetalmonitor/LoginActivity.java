7c008af55de940549759e3f38c275853d37e101c
zhanglu <1244022825@qq.com>
2018-03-31 22:57:55 +0800

    login and main page

---
+++ app/src/main/java/com/zl/administrator/fetalmonitor/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.zl.administrator.fetalmonitor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2018/3/20/020.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private long exitTime = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+
+    }
+
+
+
+
+}
