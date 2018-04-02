7c0cc562c863f3ed89c775baf2987445cefbd0d7
zhanglu <1244022825@qq.com>
2018-04-02 21:31:05 +0800

    mian compete

--- app/src/main/java/com/zl/administrator/fetalmonitor/MainActivity.java
+++ app/src/main/java/com/zl/administrator/fetalmonitor/MainActivity.java
@@ -1,13 +1,61 @@
 package com.zl.administrator.fetalmonitor;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
 
+    private RadioGroup rg_tab_bar;
+    private RadioButton rb_channel;
+
+    //Fragment Object
+    private MonitorFragment monitorFragment;
+    private HistoryFragment historyFragment;
+    private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getFragmentManager();
+        rg_tab_bar = findViewById(R.id.rg_tab_bar);
+        rg_tab_bar.setOnCheckedChangeListener(this);
+        //获取第一个单选按钮，并设置其为选中状态
+        rb_channel = findViewById(R.id.rb_monitor);
+        rb_channel.setChecked(true);
+    }
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        hideAllFragment(fragmentTransaction);
+        switch (checkedId){
+            case R.id.rb_monitor:
+                if(monitorFragment == null){
+                    monitorFragment = new MonitorFragment();
+                    fragmentTransaction.add(R.id.main_view,monitorFragment);
+                }else{
+                    fragmentTransaction.show(monitorFragment);
+                }
+                break;
+            case R.id.rb_history:
+                if(historyFragment == null){
+                    historyFragment = new HistoryFragment();
+                    fragmentTransaction.add(R.id.main_view,historyFragment);
+                }else{
+                    fragmentTransaction.show(historyFragment);
+                }
+                break;
+
+        }
+        fragmentTransaction.commit();
+    }
+
+    //隐藏所有Fragment
+    private void hideAllFragment(FragmentTransaction fragmentTransaction){
+        if(monitorFragment != null)fragmentTransaction.hide(monitorFragment);
+        if(historyFragment != null)fragmentTransaction.hide(historyFragment);
     }
 }
---
+++ app/src/main/java/com/zl/administrator/fetalmonitor/MonitorFragment.java
@@ -0,0 +1,20 @@
+package com.zl.administrator.fetalmonitor;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+/**
+ * Created by Administrator on 2018/4/2/002.
+ */
+
+public class MonitorFragment extends Fragment {
+
+    private String content;
+    public MonitorFragment() {
+    }
+
+
+}
